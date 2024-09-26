032e97e22d53732f7bda62d0e4f5c65241f0a525
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-09-26 12:10:10 +0500

    notification table

--- admin_panel/src/main.py
+++ admin_panel/src/main.py
@@ -2,7 +2,7 @@ from starlette.applications import Starlette
 from starlette_admin.contrib.sqla import Admin, ModelView
 
 from src.database import engine
-from src.models import Templates
+from src.models import Notification, Template
 
 app = Starlette()
 
@@ -11,7 +11,8 @@ admin = Admin(engine, title='Tutorials: Basic')
 
 # Adding a view for the  model
 # admin.add_view(ModelView(Todo))
-admin.add_view(ModelView(Templates, label='Шаблоны'))
+admin.add_view(ModelView(Template, label='Шаблоны'))
+admin.add_view(ModelView(Notification, label='Уведомления'))
 
 # Mount admin to my app
 admin.mount_to(app)
--- admin_panel/src/models.py
+++ admin_panel/src/models.py
@@ -1,16 +1,48 @@
+import uuid
+from abc import ABC
+from datetime import datetime
+
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import TypeDecorator
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 metadata = sa.MetaData()
+
+
+class UUIDString(TypeDecorator, ABC):
+    impl = PG_UUID(as_uuid=True)
+
+    def process_bind_param(self, value, dialect):
+        if value == '':
+            return None
+        return value
 
 
 class Base(DeclarativeBase):
     metadata = metadata
 
 
-class Templates(Base):
+class Template(Base):
     __tablename__ = 'template'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(sa.String, nullable=False, doc='Название шаблона')
-    content: Mapped[str] = mapped_column(sa.Text, nullable=True, doc='Jinja шаблон')
+    id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    content: Mapped[str] = mapped_column(sa.Text, nullable=False)
+
+    notifications: Mapped[list['Notification']] = relationship(back_populates='template')
+
+
+class Notification(Base):
+    __tablename__ = 'notification'
+
+    id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    template_id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), sa.ForeignKey('template.id'), nullable=False)
+    content_id: Mapped[uuid.UUID | None] = mapped_column(UUIDString, nullable=True)
+    content_value: Mapped[str] = mapped_column(sa.String, nullable=True)
+    last_update: Mapped[sa.DateTime] = mapped_column(sa.DateTime, nullable=False, default=datetime.now)
+    cron: Mapped[str] = mapped_column(sa.String, nullable=True)
+    users: Mapped[list[str]] = mapped_column(sa.ARRAY(sa.String), nullable=False)
+
+    template: Mapped['Template'] = relationship(back_populates='notifications')
