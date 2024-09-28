14ebf5ead0b5c0f851a09bf4577668a2f6ada5de
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-09-28 13:49:24 +0500

    views

--- admin_panel/src/main.py
+++ admin_panel/src/main.py
@@ -1,18 +1,30 @@
 from starlette.applications import Starlette
-from starlette_admin.contrib.sqla import Admin, ModelView
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
+from starlette_admin.contrib.sqla import Admin
 
+from src.auth import MyAuthProvider
 from src.database import engine
 from src.models import Notification, Template
+from src.views import NotificationView, TemplateView
 
-app = Starlette()
+app = Starlette(
+    routes=[Mount('/static', app=StaticFiles(directory='static'), name='static')],
+)
 
-# Create an empty admin interface
-admin = Admin(engine, title='Tutorials: Basic')
+admin = Admin(
+    engine,
+    title='Notifications Admin',
+    base_url='/admin',
+    statics_dir='static',
+    login_logo_url='/admin/statics/logo.svg',  # base_url + '/statics/' + path_to_the_file
+    auth_provider=MyAuthProvider(),
+    middlewares=[Middleware(SessionMiddleware, secret_key='my_super_secret_key')],
+)
 
-# Adding a view for the  model
-# admin.add_view(ModelView(Todo))
-admin.add_view(ModelView(Template, label='Шаблоны'))
-admin.add_view(ModelView(Notification, label='Уведомления'))
+admin.add_view(TemplateView(Template, label='Шаблоны'))
+admin.add_view(NotificationView(Notification, label='Уведомления'))
 
-# Mount admin to my app
 admin.mount_to(app)
