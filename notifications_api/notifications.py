2caea18b7fa17b8e9f6fe23ac031b987d77401b3
Дмитрий Малашихин <themdq@yandex.ru>
2024-10-02 01:31:49 +0300

    added base structure

---
+++ notifications_api/notifications.py
@@ -0,0 +1,42 @@
+import logging
+import pickle
+from datetime import datetime
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from pydantic import BaseModel, Field
+
+router = APIRouter()
+producer = KafkaProducer(bootstrap_servers=settings.kafka.bootstrap_servers)
+logger = logging.getLogger(__name__)
+
+
+class NotificationModel(BaseModel):
+    event_id: str = Field(description='Идентификатор события')
+    event_type: str = Field(description='Тип события (email)')
+    event_at: datetime | None = Field(description='Время отправки события')
+    template_id: str = Field(description='Идентификатор шаблона')
+    template_name: str = Field(description='Название шаблона')
+    template_content: str = Field(description='Содержимое шаблона')
+    payload_for_template: dict = Field(description='Данные для шаблона')
+    user_ids: list = Field(description='Идентификатор пользователя')
+    cron: str | None = Field(description='Запуск расписания')
+    created_at: datetime = Field(description='Время создания события')
+    updated_at: datetime = Field(description='Время обновления события')
+
+
+@router.post('/', description='Send notification to Kafka')
+async def record_notification(notification: NotificationModel):
+    try:
+        result = producer.send(
+            settings.kafka.topic,
+            key=pickle.dumps(notification.event_type),
+            value=pickle.dumps(notification.model_dump_json()),
+        )
+        producer.flush()
+        logger.info('Сообщение доставлено в %s [partition %s]', result.get().topic, result.get().partition)
+        return {'status': 'success', 'message': 'Event recorded and sent to Kafka.'}
+    except KafkaError as e:
+        logger.error('Ошибка доставки сообщения: %s', e)
+        return HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
