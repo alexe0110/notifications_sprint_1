3f101e98f957895dadb63c607e007b2c56388441
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-10-02 12:08:09 +0500

    fix notification table

--- admin_panel/alembic/env.py
+++ admin_panel/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
@@ -8,6 +9,13 @@ from src.models import metadata
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, 'POSTGRES_USER', os.getenv('POSTGRES_USER'))
+config.set_section_option(section, 'POSTGRES_PASSWORD', os.getenv('POSTGRES_PASSWORD'))
+config.set_section_option(section, 'POSTGRES_HOST', os.getenv('POSTGRES_HOST'))
+config.set_section_option(section, 'POSTGRES_PORT', os.getenv('POSTGRES_PORT'))
+config.set_section_option(section, 'POSTGRES_DB', os.getenv('POSTGRES_DB'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- admin_panel/src/models.py
+++ admin_panel/src/models.py
@@ -6,6 +6,7 @@ import sqlalchemy as sa
 from sqlalchemy import TypeDecorator
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
 
 metadata = sa.MetaData()
 
@@ -37,12 +38,16 @@ class Notification(Base):
     __tablename__ = 'notification'
 
     id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name: Mapped[str] = mapped_column(sa.String, nullable=False)
-    template_id: Mapped[uuid.UUID] = mapped_column(PG_UUID(as_uuid=True), sa.ForeignKey('template.id'), nullable=False)
-    content_id: Mapped[uuid.UUID | None] = mapped_column(UUIDString, nullable=True)
-    content_value: Mapped[str] = mapped_column(sa.String, nullable=True)
-    last_update: Mapped[sa.DateTime] = mapped_column(sa.DateTime, nullable=False, default=datetime.now)
-    cron: Mapped[str] = mapped_column(sa.String, nullable=True)
+    type: Mapped[str] = mapped_column(sa.String, nullable=False, server_default='email')
+    payload: Mapped[dict] = mapped_column(sa.JSON, nullable=True)
     users: Mapped[list[str]] = mapped_column(sa.ARRAY(sa.String), nullable=False)
+    event_at: Mapped[sa.DateTime] = mapped_column(sa.DateTime, nullable=True, default=datetime.now)
+    cron: Mapped[str] = mapped_column(sa.String, nullable=True)
+    updated_at: Mapped[sa.DateTime] = mapped_column(
+        sa.DateTime, nullable=False, default=datetime.now, server_default=func.now()
+    )
+    created_at: Mapped[sa.DateTime] = mapped_column(
+        sa.DateTime, nullable=False, default=datetime.now, server_default=func.now()
+    )
 
     template: Mapped['Template'] = relationship(back_populates='notifications')
