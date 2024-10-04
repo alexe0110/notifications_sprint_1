c836727dcd7b586371a7def7aa72d840151c6e01
nafedoseev <nafedoseev@domclick.ru>
2024-10-04 19:19:32 +0300

    inc workers

--- notification_worker/run.sh
+++ notification_worker/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec gunicorn -w 1 --bind 0.0.0.0:8008 --worker-class uvicorn.workers.UvicornWorker  --log-level INFO --access-logfile=- 'main:app'
+exec gunicorn -w 4 --bind 0.0.0.0:8008 --worker-class uvicorn.workers.UvicornWorker  --log-level INFO --access-logfile=- 'main:app'
